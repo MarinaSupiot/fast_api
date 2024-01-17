de20386ab4cf7f3a99848aeaa786d377c7dc0835
MarinaSupiot <102304229+MarinaSupiot@users.noreply.github.com>
2024-01-17 10:32:44 +0100

    myapp 1000

--- myapp.py
+++ myapp.py
@@ -6,6 +6,7 @@ import joblib
 from io import BytesIO
 from zipfile import ZipFile
 import aiohttp
+from aiohttp import ClientSession
 
 app = FastAPI()
 
@@ -13,14 +14,16 @@ async def load_data():
     try:
         zip_url = "https://github.com/MarinaSupiot/fast_api/raw/main/test_preprocess_reduit.csv.zip"
         
-        async with aiohttp.ClientSession() as session:
+        async with ClientSession() as session:
             async with session.get(zip_url) as response:
                 content = await response.read()
-        
+
+        # Используйте асинхронную библиотеку для работы с ZIP-архивом, если доступно
         with ZipFile(BytesIO(content), 'r') as zip_file:
             csv_file_name = 'test_preprocess_reduit.csv'
             with zip_file.open(csv_file_name) as file:
-                df_test = pd.read_csv(file)
+                # Уменьшите количество данных при необходимости
+                df_test = pd.read_csv(file, nrows=1000)  # пример: загрузить первые 1000 строк
 
         return df_test
     except Exception as e:
